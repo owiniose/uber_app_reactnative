ebeefb85c83f7a54d96a11248236efe23e2e89a8
Owini <owiniose@gmail.com>
2022-12-30 12:39:21 +0100

    commit

---
+++ components/NavOptions.js
@@ -0,0 +1,63 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from 'react-native'
+import React from 'react';
+import tw from 'tailwind-react-native-classnames'
+import uberx from '../assets/UberX.webp'
+import ubere from '../assets/eats.png';
+import { Icon } from '@rneui/base';
+import { useNavigation } from '@react-navigation/native';
+
+
+
+const data =[
+    {
+        id: "123",
+        title: "Get a ride",
+        image: uberx,
+        screen: "MapScreen",
+    },
+    {
+        id: "456",
+        title: "Order Food",
+        image: ubere,
+        screen: "EatsScreen",//Change in Future
+    },
+];
+
+const NavOptions = () => {
+
+    const navigation = useNavigation();
+
+  return (
+   <FlatList
+    data={data}
+    keyExtractor={(item)=> item.id}
+    horizontal
+    renderItem={({item})=>(
+        <TouchableOpacity
+            onPress={()=> navigation.navigate(item.screen)} 
+            style ={tw` p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            <View>
+                <Image
+                style={{
+                    width: 120, height: 120, resizeMode: 'contain'
+                }}
+                source={item.image}
+                />
+                <Text style={tw` mt-2 text-lg font-semibold`}>
+                    {item.title}
+                </Text>
+                <Icon
+                style={tw` p-2 bg-black rounded-full w-10 mt-4`}
+                name='arrowright'
+                type='antdesign'
+                color='white'
+                />
+
+            </View>
+        </TouchableOpacity>
+    )}  
+   />
+  )
+}
+
+export default NavOptions
---
+++ screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+import uberlogo from '../assets/uber_logo.png'
+import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_KEY} from '@env';
+
+
+
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+        <View style={tw`p-5`}>
+
+           <Image
+           style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+           }} 
+           source={uberlogo}
+           />
+           <GooglePlacesAutocomplete
+            styles={{
+               container: {
+                  flex: 0
+               },
+               textInput:{
+                  fontSize: 18,
+               }
+            }}
+            onPress={(data, details = null ) => {
+               console.log(details);
+               console.log(data);
+            } 
+         }
+            fetchDetails={true}
+            returnKeyType={"search"}
+            enablePoweredByContainer={false}
+            query={{
+               key:GOOGLE_MAPS_KEY,
+               language: 'en',
+            }}
+           nearbyPlacesAPI='GooglePlacesSearch'
+           debounce={400}
+           placeholder="Where From?"
+           />
+           <NavOptions/>
+        </View>
+     </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles = StyleSheet.create({})
