5526fbe29b36e6d2f8b6d130c51b1ca78a6f45a4
Owini <owiniose@gmail.com>
2023-01-11 16:16:24 +0100

    new

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,36 +1,84 @@
 import { StyleSheet, Text, SafeAreaView, View,
-     TouchableOpacity } from 'react-native'
+     TouchableOpacity, 
+     FlatList} from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames'
-import { Icon } from 'react-native-elements'
+import { Icon, Image } from 'react-native-elements'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
+
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "Uber XL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "Uber LUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 
 const RideOptionsCard = () => {
 
    const navigation = useNavigation();
+   const [selected, setSelected] = useState(null);
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
-        <View style={tw`flex-row py-12 px-6 items-center`}>
+        <View style={tw`flex-row py-6 px-6 items-center`}>
             <TouchableOpacity onPress={()=> navigation.navigate("NavigateCard")}
             >
                 <Icon
                 name='chevron-left'
                 type='font-awesome'
                 />
-      </TouchableOpacity>
-      {/* <TouchableOpacity style= {tw`flex 
-      flex-row justify-between bg-black w-24 px-4 py-3
-       rounded-full`}
-       onPress={()=> navigation.navigate("NavigateCard")}
-       >
-          <Icon
-         name='chevron-left'
-         type='font-awesome' color='white' size={16}
-          />
-          
-      </TouchableOpacity> */}
+      </TouchableOpacity> 
       <Text style={tw`text-center text-xl pl-28`}>Select a Ride</Text>
+        </View>
+        <FlatList data={data}
+          keyExtractor={(item)=>item.id}
+          renderItem={({item:{id, title, multiplier, image}, item }) =>(
+            
+            <TouchableOpacity  onPress ={()=> setSelected(item)}
+            style={tw`flex-row justify-between 
+            px-10 items-center ${id === selected?.id && 'bg-gray-200'}`}
+             
+            >
+              <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain"
+              }}
+              source ={{uri : image}}
+              />
+              <View style={tw`-ml-6`}>
+                <Text style={tw`text-xl font-semibold`}>{title}</Text>
+                <Text>Travel time...</Text>
+              </View>
+              <Text style={tw`text-xl`}>
+                $99
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+
+        <View>
+          <TouchableOpacity style={tw`bg-black py-3 m-3`}> 
+            <Text style={tw`text-center text-white text-xl`}>
+              Choose {selected?.title}
+            </Text>
+          </TouchableOpacity>
         </View>
     </SafeAreaView>
   )
